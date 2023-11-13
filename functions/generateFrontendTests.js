f30115677096e333fa98ec178b679c727b9d73cd
sasle <sasle57@gmail.com>
2023-11-12 21:34:35 -0300

    frontend jest

--- functions/generateFrontendTests.js
+++ functions/generateFrontendTests.js
@@ -52,13 +52,43 @@ function sum(a, b) {
 
     console.log(`${GREEN}Configured Vitest for the frontend.${RESET}`);
   } else {
-    shell.exec(
-      "npm install @types/jest@latest ts-jest@latest jest@latest supertest@latest @types/supertest@latest",
-      {
-        stdio: "inherit",
-        silent: true,
+    shell.exec("npm install @types/jest@latest ts-jest@latest jest@latest", {
+      stdio: "inherit",
+      silent: true,
+    });
+
+    // Create a basic Jest test file
+    const testContents = `
+      test('adds 1 + 2 to equal 3', () => {
+        expect(sum(1, 2)).toBe(3);
+      });
+
+      function sum(a, b) {
+        return a + b;
       }
+    `;
+
+    // Create a folder for tests if it doesn't exist
+    if (!shell.test("-d", "__tests__")) {
+      shell.mkdir("__tests__");
+    }
+
+    // Save the basic Jest test file
+    shell.ShellString(testContents).to("__tests__/basic.test.ts");
+
+    // Update the "test" script in package.json
+    const packageJsonPath = path.join(projectPath, "client/package.json");
+    const packageJsonContent = JSON.parse(
+      readFileSync(packageJsonPath, "utf-8")
     );
+
+    packageJsonContent.scripts = {
+      ...packageJsonContent.scripts,
+      test: "jest",
+    };
+
+    writeFileSync(packageJsonPath, JSON.stringify(packageJsonContent, null, 2));
+
     console.log(`${GREEN}Configured Jest for the frontend.${RESET}`);
   }
 }
